f97768fb2c0b41f16ff3d569787bfe6821716fdd
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-30 13:54:10 +0100

    Use all defaults

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
-#include <cassert>
 #include <iostream>
-void f()
-{
-std::cout << "Hello";
- std::cout << "Hello";
-  std::cout << "Hello";
-   std::cout << "Hello";
-    std::cout << "Hello";
-}
 
 int main() 
 {
     static_assert(1 + 1 == 2); //C++17
-  #ifdef NDEBUG
-  #error Must use scan-build in debug mode
-  #endif
-  int a[3] = { 0, 1, 2 };
-  a[4] = 0; // Access violation!
-  std::cout
-    << a[4] //Access violation!
-  ;
+    std::cout << "Hello";
+    std::cout << "misindented";
+    std::cout << "world";
 }
