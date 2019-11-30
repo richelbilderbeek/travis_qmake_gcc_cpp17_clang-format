42aa22d296987b779b2eb928ecb50ddde45c5b4d
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-30 15:02:35 +0100

    Indent of 2 is standard

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main() 
 {
-    static_assert(1 + 1 == 2); //C++17
-    std::cout << "Hello";
-    std::cout << "misindented";
-    std::cout << "world";
+  static_assert(1 + 1 == 2); //C++17
+  std::cout << "Hello";
+   std::cout << "misindented";
+  std::cout << "world";
 }
--- main_ok.cpp
+++ main_ok.cpp
@@ -2,8 +2,8 @@
 
 int main() 
 {
-    static_assert(1 + 1 == 2); //C++17
-    std::cout << "Hello";
-    std::cout << "correctly indented";
-    std::cout << "world";
+  static_assert(1 + 1 == 2); //C++17
+  std::cout << "Hello";
+  std::cout << "correctly indented";
+  std::cout << "world";
 }
